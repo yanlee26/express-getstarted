402edf4ff30b1a83dfa62fd158a5c2ff0e037aab
Yan,Li <yanli@peilian.com>
2019-01-10 19:26:10 +0800

    add node-js-sample

--- index.js
+++ index.js
@@ -1,53 +1,15 @@
-var express = require('express');
-
+var express = require('express')
 var app = express();
 
-// set up handlebars view engine
-var handlebars = require('express-handlebars')
-  .create({
-    defaultLayout: 'index'
-  });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+app.set('port', (process.env.PORT || 5000))
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public');
 
-app.set('port', process.env.PORT || 3000);
+app.get('/', function(request, response) {
+  response.render('index.html')
+})
 
-app.use(express.static(__dirname + '/public'));
-
-var fortuneCookies = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-];
-
-app.get('/', function (req, res) {
-  // aa
-  res.render('home');
-});
-app.get('/about', function (req, res) {
-  var randomFortune =
-    fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-  res.render('about', {
-    fortune: randomFortune
-  });
-});
-
-// 404 catch-all handler (middleware)
-app.use(function (req, res, next) {
-  res.status(404);
-  res.render('404');
-});
-
-// 500 error handler (middleware)
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
-});
-
-app.listen(app.get('port'), function () {
-  console.log('Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.');
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
